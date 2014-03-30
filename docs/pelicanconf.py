084ad6ed14ed0fcd9ace812af299d22bf7ee35cf
Vítor Avelino <contact@vitoravelino.me>
2014-03-30 19:55:22 -0300

    WIP of docs using pelican

---
+++ docs/pelicanconf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'buddycloud'
+SITENAME = u'buddycloud styles'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+THEME = 'theme'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
